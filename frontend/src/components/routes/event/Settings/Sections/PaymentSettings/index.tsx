45001e7bf1d6ff245913f9dfce2c017c540d1856
Dirk <dschaap1@gmail.com>
2025-05-15 15:13:39 +0000

    test

--- frontend/src/components/routes/event/Settings/Sections/PaymentSettings/index.tsx
+++ frontend/src/components/routes/event/Settings/Sections/PaymentSettings/index.tsx
@@ -86,6 +86,11 @@ export const PaymentAndInvoicingSettings = () => {
             description: t`Accept credit card payments with Stripe`
         },
         {
+            value: "Mollie",
+            label: t`Mollie`,
+            description: t`Accept credit card payments with Mollie`
+        },
+        {
             value: "OFFLINE",
             label: t`Offline Payments`,
             description: t`Accept bank transfers, checks, or other offline payment methods`
